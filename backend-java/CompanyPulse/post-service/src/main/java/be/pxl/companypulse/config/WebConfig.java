30150e6a8cb8a0d74c1422e253f68f985e2a3bb7
BenteBussePXL <bente.busse@student.pxl.be>
2025-01-12 20:50:11 +0100

    Added dockerized angular app

--- backend-java/CompanyPulse/post-service/src/main/java/be/pxl/companypulse/config/WebConfig.java
+++ backend-java/CompanyPulse/post-service/src/main/java/be/pxl/companypulse/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(false);
--- frontend-web/company-pulse/src/environments/environment.ts
+++ frontend-web/company-pulse/src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    postUrl: 'http://localhost:8000/post/posts',
+    reviewUrl: 'http://localhost:8000/review/reviews',
+    notificationUrl: 'http://localhost:8000/notification/notifications',
+    commentUrl: 'http://localhost:8000/comment/comments',
+};
